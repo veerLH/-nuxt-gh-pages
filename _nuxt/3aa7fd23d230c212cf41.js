3136282b0746db394f6ec90bddaadbf4a113bb08
Veer <zinlynnhtun7979@gmail.com>
2020-02-14 14:44:22 -0800

    Updates

---
+++ _nuxt/3aa7fd23d230c212cf41.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{288:function(n,t,e){"use strict";e.r(t);var c={},o=e(31),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{},[this._v("\n  About \n")])}),[],!1,null,"bdc774e2",null);t.default=component.exports}}]);
